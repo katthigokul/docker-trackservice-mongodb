cd90aebb0ebd606f0f664a512fc775c471be70e6
katthigokul <gokulkatthi@gmail.com>
2019-08-07 17:53:28 +0530

    created  docker for mongo

---
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("application-prod.properties")
+@SpringBootApplication
+public class TrackServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication. run(TrackServiceApplication.class, args);
+    }
+}
